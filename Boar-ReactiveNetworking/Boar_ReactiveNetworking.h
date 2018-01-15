620d0e50a15f4cce758628cd6fb072b331c70f52
Peter <peter@devteamforyou.com>
2018-01-15 21:06:50 +0200

    Added carthage

---
+++ Boar-ReactiveNetworking/Boar_ReactiveNetworking.h
@@ -0,0 +1,19 @@
+//
+//  Boar_ReactiveNetworking.h
+//  Boar-ReactiveNetworking
+//
+//  Created by Peter Ovchinnikov on 1/15/18.
+//  Copyright © 2018 Peter Ovchinnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Boar_ReactiveNetworking.
+FOUNDATION_EXPORT double Boar_ReactiveNetworkingVersionNumber;
+
+//! Project version string for Boar_ReactiveNetworking.
+FOUNDATION_EXPORT const unsigned char Boar_ReactiveNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Boar_ReactiveNetworking/PublicHeader.h>
+
+
